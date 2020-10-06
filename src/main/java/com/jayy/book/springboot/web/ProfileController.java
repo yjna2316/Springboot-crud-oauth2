64e4fbf3019bad643860f79941330dac95767a18
Yoonji Na <yjna2316@naver.com>
2020-10-06 18:02:19 +0900

    profile API 및 테스트 코드 추가

---
+++ src/main/java/com/jayy/book/springboot/web/ProfileController.java
@@ -0,0 +1,29 @@
+package com.jayy.book.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ProfileController {
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String profile() {
+        // getActiveProfiles() : 현재 실행중인 ActiveProfile을 모두 가져온다.
+        // real, oauth, real-db 등이 활성화 되어 있다면(active) 3개가 모두 담겨 있다.
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+}
